8f0b42465f926d69007ce4bb0ea3fb09efac4904
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-09 21:21:46 +0300

    Changed the notation of the image

--- SimpleAnimationSelector/script.js
+++ SimpleAnimationSelector/script.js
@@ -5,4 +5,4 @@ const CANVAS_WIDTH = canvas.width = 600
 const CANVAS_HEIGHT = canvas.height = 600
 
 const playerImage = new Image()
-playerImage.src('./images/shadow_dog.png')
+playerImage.src = './images/shadow_dog.png'
