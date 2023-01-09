8d1712ecc97bac5ee47e700cac6c4875c0e139bf
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-09 21:17:39 +0300

    Defined the canvas_width and the canvas_height constants

--- SimpleAnimationSelector/script.js
+++ SimpleAnimationSelector/script.js
@@ -1,4 +1,5 @@
 const canvas = document.getElementById('canvas1')
 const ctx = canvas.getContext('2d')
 
-console.log(ctx)
+const CANVAS_WIDTH = canvas.width = 600
+const CANVAS_HEIGHT = canvas.height = 600
