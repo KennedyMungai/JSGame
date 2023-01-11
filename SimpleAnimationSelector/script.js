513c967cc25fdb287d2eb349d322475871c5aa00
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-11 20:08:15 +0300

    Called another drawImage function

--- SimpleAnimationSelector/script.js
+++ SimpleAnimationSelector/script.js
@@ -11,6 +11,7 @@ const animate = () =>
 {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
     ctx.fillRect(50, 50, 100, 100)
+    ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh)
     ctx.drawImage(playerImage, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
     requestAnimationFrame(animate)
 }
