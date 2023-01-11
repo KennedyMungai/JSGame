8435c22da2f9916acb430bc6697a39aedbc1d381
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-11 22:52:01 +0300

    Defined the 5 layers of the background

--- ParallaxScrolling/app.js
+++ ParallaxScrolling/app.js
@@ -6,3 +6,11 @@ let gameSpeed = 5
 
 const backgroundLayer1 = new Image()
 backgroundLayer1.src = './images/background/layer-1.png'
+const backgroundLayer2 = new Image()
+backgroundLayer1.src = './images/background/layer-2.png'
+const backgroundLayer3 = new Image()
+backgroundLayer1.src = './images/background/layer-3.png'
+const backgroundLayer4 = new Image()
+backgroundLayer1.src = './images/background/layer-4.png'
+const backgroundLayer5 = new Image()
+backgroundLayer1.src = './images/background/layer-5.png'
