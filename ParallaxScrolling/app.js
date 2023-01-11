35d83ec101a97774e944e25a3023e1770da51edc
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-11 23:10:31 +0300

    Added some scrolling code to the image

--- ParallaxScrolling/app.js
+++ ParallaxScrolling/app.js
@@ -22,14 +22,24 @@ const animate = () =>
 {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
     ctx.drawImage(backgroundLayer4, x, 0)
+    ctx.drawImage(backgroundLayer4, x2, 0)
 
     if (x < -2400)
     {
-        x = 0
+        x = 2400
     }
     else
     {
         x -= gameSpeed
+    }
+
+    if (x2 < -2400)
+    {
+        x2 = 2400
+    }
+    else
+    {
+        x2 -= gameSpeed
     }
 
     requestAnimationFrame(animate)
