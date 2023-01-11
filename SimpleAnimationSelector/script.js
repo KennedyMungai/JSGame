9cecd72ab7c4181ab261ee1be23c644e7d22a466
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-11 20:15:38 +0300

    Used the proper cropped image sizes

--- SimpleAnimationSelector/script.js
+++ SimpleAnimationSelector/script.js
@@ -4,6 +4,9 @@ const ctx = canvas.getContext('2d')
 const CANVAS_WIDTH = canvas.width = 600
 const CANVAS_HEIGHT = canvas.height = 600
 
+const spriteWidth = 575
+const spriteHeight = 525
+
 const playerImage = new Image()
 playerImage.src = './images/shadow_dog.png'
 
@@ -12,7 +15,7 @@ const animate = () =>
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
     ctx.fillRect(50, 50, 100, 100)
     // ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh)
-    ctx.drawImage(playerImage, 0, 0, 700, 700, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+    ctx.drawImage(playerImage, 0, 0, spriteWidth, spriteHeight, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
     requestAnimationFrame(animate)
 }
 
