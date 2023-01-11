5e5a90fc238ae82e62f768595530d367d6a985d8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-11 23:03:48 +0300

    Added some code to make the image scroll infinitly

--- ParallaxScrolling/app.js
+++ ParallaxScrolling/app.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById('canvas1')
 const ctx = canvas.getContext('2d')
 const CANVAS_WIDTH = canvas.width = 800
 const CANVAS_HEIGHT = canvas.height = 700
-let gameSpeed = 5
+let gameSpeed = 1
 
 const backgroundLayer1 = new Image()
 backgroundLayer1.src = './images/background/layer-1.png'
@@ -21,7 +21,16 @@ const animate = () =>
 {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
     ctx.drawImage(backgroundLayer4, x, 0)
-    x -= gameSpeed
+
+    if (x < -1000)
+    {
+        x = 0
+    }
+    else
+    {
+        x -= gameSpeed
+    }
+
     requestAnimationFrame(animate)
 }
 
