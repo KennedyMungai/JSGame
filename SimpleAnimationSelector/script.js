4ca71c7052949debd9e22acdfa1342716d6850bb
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-11 21:25:19 +0300

    Added all the other animation states

--- SimpleAnimationSelector/script.js
+++ SimpleAnimationSelector/script.js
@@ -22,6 +22,38 @@ const animationStates = [
     {
         name: 'jump',
         frames: 7
+    },
+    {
+        name: 'fall',
+        frames: 9
+    },
+    {
+        name: 'run',
+        frames: 9
+    },
+    {
+        name: 'dizzy',
+        frames: 11
+    },
+    {
+        name: 'sit',
+        frames: 5
+    },
+    {
+        name: 'roll',
+        frames: 7
+    },
+    {
+        name: 'bite',
+        frames: 7
+    },
+    {
+        name: 'ko',
+        frames: 12
+    },
+    {
+        name: 'getHit',
+        frames: 4
     }
 ]
 
