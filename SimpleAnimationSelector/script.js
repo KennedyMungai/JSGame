4c9c47a8b4b90c2be3ab1f689ede3594466b5e68
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-11 21:17:12 +0300

    Deleted the definitions of the frameX and the frameY variables

--- SimpleAnimationSelector/script.js
+++ SimpleAnimationSelector/script.js
@@ -7,9 +7,6 @@ const CANVAS_HEIGHT = canvas.height = 600
 const spriteWidth = 575
 const spriteHeight = 523
 
-let frameX = 0
-let frameY = 0
-
 let gameFrame = 0
 const staggerFrames = 5
 
@@ -50,7 +47,7 @@ const animate = () =>
 {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
     let position = Math.floor(gameFrame / staggerFrames) % spriteAnimations['idle'].loc.length
-    frameX = spriteWidth * position
+    let frameX = spriteWidth * position
     ctx.drawImage(playerImage, frameX, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight)
     gameFrame++
 
