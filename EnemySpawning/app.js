7d3ac852eea1b7a75f89436d3cf3bf709b880e2c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-12 14:09:00 +0300

    Assigned the dimensions of the canvas element to a global variable

--- EnemySpawning/app.js
+++ EnemySpawning/app.js
@@ -1,3 +1,5 @@
 /** @type {HTMLCanvasElement} */
 const canvas = document.getElementById('canvas1')
 const ctx = canvas.getContext('2d')
+const CANVAS_WIDTH = canvas.width = 600
+const CANVAS_HEIGHT = canvas.height = 500
