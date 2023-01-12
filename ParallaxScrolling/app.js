e3b5ff79a6e3d47e8f7b512a5b16600d4a172315
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-12 12:03:17 +0300

    Got rid of some more logic

--- ParallaxScrolling/app.js
+++ ParallaxScrolling/app.js
@@ -19,25 +19,6 @@ const animate = () =>
 {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
     ctx.drawImage(backgroundLayer4, x, 0)
-    ctx.drawImage(backgroundLayer4, x2, 0)
-
-    if (x < -2400)
-    {
-        x = 2400 + x2 - gameSpeed
-    }
-    else
-    {
-        x -= gameSpeed
-    }
-
-    if (x2 < -2400)
-    {
-        x2 = 2400 + x - gameSpeed
-    }
-    else
-    {
-        x2 -= gameSpeed
-    }
 
     requestAnimationFrame(animate)
 }
