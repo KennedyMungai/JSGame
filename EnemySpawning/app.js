e51b2b9694ced1ffd25d63a120c66ce1137ec86b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-12 22:25:18 +0300

    Commented out the older code for dcumebtration purposes

--- EnemySpawning/app.js
+++ EnemySpawning/app.js
@@ -210,6 +210,91 @@
 // animate()
 
 
+// /** @type {HTMLCanvasElement} */
+// const canvas = document.getElementById('canvas1')
+// const ctx = canvas.getContext('2d')
+// const CANVAS_WIDTH = canvas.width = 600
+// const CANVAS_HEIGHT = canvas.height = 500
+// const numberOfEnemies = 50
+// const enemiesArray = []
+
+// let gameFrame = 0
+
+// class Enemy 
+// {
+//     constructor()
+//     {
+//         this.image = new Image()
+//         this.image.src = './images/enemy_ghost_1.png'
+//         this.speed = Math.random() * 4 + 1
+//         this.spriteWidth = 217
+//         this.spriteHeight = 177
+//         this.width = this.spriteWidth / 2
+//         this.height = this.spriteHeight / 2
+//         this.x = Math.random() * (CANVAS_WIDTH - this.width)
+//         this.y = Math.random() * (CANVAS_HEIGHT - this.height)
+//         this.newX = Math.random() * (CANVAS_WIDTH - this.width)
+//         this.newY = Math.random() * (CANVAS_HEIGHT - this.height)
+//         this.frame = 0
+//         this.flapSpeed = Math.floor(Math.random() * 3 + 1)
+//         this.interval = Math.floor(Math.random() * 200 + 50)
+//     }
+
+//     update()
+//     {
+//         if (gameFrame % this.interval === 0)
+//         {
+//             this.newX = Math.random() * (CANVAS_WIDTH - this.width)
+//             this.newY = Math.random() * (CANVAS_HEIGHT - this.height)
+//         }
+
+//         let dx = this.x - this.newX
+//         let dy = this.y - this.newY
+
+//         this.x -= dx / 60
+//         this.y -= dy / 60
+
+//         // this.x = 0
+//         // this.y = 0
+//         this.angle += this.angleSpeed
+//         // animate frames
+
+//         if (this.x + this.width < 0) this.x = canvas.width
+
+//         if (gameFrame % this.flapSpeed === 0)
+//         {
+//             this.frame > 4 ? this.frame = 0 : this.frame++
+//         }
+//     }
+
+//     draw()
+//     {
+//         ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height)
+//     }
+// }
+
+// for (let i = 0; i < numberOfEnemies; i++)
+// {
+//     enemiesArray.push(new Enemy())
+// }
+
+// const animate = () => 
+// {
+//     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+
+//     enemiesArray.forEach((enemy) =>
+//     {
+//         enemy.update()
+//         enemy.draw()
+//     })
+//     gameFrame++
+
+//     requestAnimationFrame(animate)
+// }
+
+// animate()
+
+
 /** @type {HTMLCanvasElement} */
 const canvas = document.getElementById('canvas1')
 const ctx = canvas.getContext('2d')
