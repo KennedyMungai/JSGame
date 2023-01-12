2c3af502737262fe4b359c9c316deb9359206d2f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-12 12:21:16 +0300

    Wrote some update code

--- ParallaxScrolling/app.js
+++ ParallaxScrolling/app.js
@@ -31,7 +31,17 @@ class Layer
 
     update()
     {
+        this.speed = gameSpeed * this.speedModifier
 
+        if (this.x <= -this.width)
+        {
+            this.x = this.width + this.x2 - this.speed
+        }
+
+        if (this.x2 <= -this.width)
+        {
+            this.x2 = this.width + this.x - this.speed
+        }
     }
 
     draw()
