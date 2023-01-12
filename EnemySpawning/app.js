da305dc3d9445d9b73d1b47175a3259f1ab0cfc3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-12 14:31:31 +0300

    Commented out the enemy1 object

--- EnemySpawning/app.js
+++ EnemySpawning/app.js
@@ -4,17 +4,18 @@ const ctx = canvas.getContext('2d')
 const CANVAS_WIDTH = canvas.width = 600
 const CANVAS_HEIGHT = canvas.height = 500
 
-enemy1 = {
-    x: 0,
-    y: 0,
-    width: 200,
-    height: 200
-}
+// enemy1 = {
+//     x: 0,
+//     y: 0,
+//     width: 100,
+//     height: 100
+// }
 
 const animate = () => 
 {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
     enemy1.x++
+    enemy1.y++
     ctx.fillRect(enemy1.x, enemy1.y, enemy1.width, enemy1.height)
 
     requestAnimationFrame(animate)
