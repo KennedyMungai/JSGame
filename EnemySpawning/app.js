629019d5c6c024f6e505de81a6c228f2dc776511
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-12 14:54:36 +0300

    Deleted some commented code

--- EnemySpawning/app.js
+++ EnemySpawning/app.js
@@ -37,8 +37,7 @@ for (let i = 0; i < numberOfEnemies; i++)
 const animate = () => 
 {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
-    // enemy1.update()
-    // enemy1.draw()
+
     enemiesArray.forEach((enemy) =>
     {
         enemy.update()
