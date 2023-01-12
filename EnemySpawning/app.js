9543f6c8b6ebecf511a733128e37828799f7afde
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-12 14:45:05 +0300

    Defined the number of enemies const and set them to 100

--- EnemySpawning/app.js
+++ EnemySpawning/app.js
@@ -3,6 +3,7 @@ const canvas = document.getElementById('canvas1')
 const ctx = canvas.getContext('2d')
 const CANVAS_WIDTH = canvas.width = 600
 const CANVAS_HEIGHT = canvas.height = 500
+const numberOfEnemies = 100
 
 class Enemy 
 {
