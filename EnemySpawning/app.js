edd6bf444eeb6a22ff5985186031853afae1b369
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-12 22:23:07 +0300

    Added more enemies and turned it into a swarm

--- EnemySpawning/app.js
+++ EnemySpawning/app.js
@@ -215,7 +215,7 @@ const canvas = document.getElementById('canvas1')
 const ctx = canvas.getContext('2d')
 const CANVAS_WIDTH = canvas.width = 600
 const CANVAS_HEIGHT = canvas.height = 500
-const numberOfEnemies = 20
+const numberOfEnemies = 100
 const enemiesArray = []
 
 let gameFrame = 0
