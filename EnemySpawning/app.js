ebc6cca3b6679b7a3607b425758314ffe0ebf8c2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-12 21:24:21 +0300

    Made the initail angle a number between 0 and 2

--- EnemySpawning/app.js
+++ EnemySpawning/app.js
@@ -89,7 +89,7 @@ class Enemy
         this.y = Math.random() * (CANVAS_HEIGHT - this.height)
         this.frame = 0
         this.flapSpeed = Math.floor(Math.random() * 3 + 1)
-        this.angle = 0
+        this.angle = Math.random() * 2
     }
 
     update()
