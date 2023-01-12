bf0666b179f9c6918f1d14544f20fb80a663b8c8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-12 12:30:21 +0300

    Created layer instances for all 5 layers

--- ParallaxScrolling/app.js
+++ ParallaxScrolling/app.js
@@ -54,7 +54,11 @@ class Layer
     }
 }
 
-const layer4 = new Layer(backgroundLayer4, 0.5)
+const layer1 = new Layer(backgroundLayer1, 1)
+const layer2 = new Layer(backgroundLayer2, 1)
+const layer3 = new Layer(backgroundLayer3, 1)
+const layer4 = new Layer(backgroundLayer4, 1)
+const layer5 = new Layer(backgroundLayer5, 1)
 
 const animate = () =>
 {
