da99d71ee307208ef7600a5f89480934390acfc1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-12 14:10:25 +0300

    Gave rthe position data to the enemy1 object

--- EnemySpawning/app.js
+++ EnemySpawning/app.js
@@ -3,3 +3,10 @@ const canvas = document.getElementById('canvas1')
 const ctx = canvas.getContext('2d')
 const CANVAS_WIDTH = canvas.width = 600
 const CANVAS_HEIGHT = canvas.height = 500
+
+enemy1 = {
+    x: 0,
+    y: 0,
+    width: 200,
+    height: 200
+}
