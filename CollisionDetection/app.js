f0a1ab33fa3defa005ffb51ad544a3daa246f0a2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-15 10:51:32 +0300

    Called the fillRect method

--- CollisionDetection/app.js
+++ CollisionDetection/app.js
@@ -4,3 +4,6 @@ const canvas = document.getElementById('canvas1')
 const ctx = canvas.getContext('2d')
 canvas.width = 500
 canvas.height = 700
+
+ctx.fillStyle = 'white'
+ctx.fillRect(50, 50, 100, 150)
