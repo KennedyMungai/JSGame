3ed0b564c4147729e88d484c6db5ecdf4b00193f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-15 12:55:08 +0300

    Defined teh Raven class and added a constructor for it

--- SideScroller/app.js
+++ SideScroller/app.js
@@ -6,3 +6,16 @@ canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
 let ravens = []
+
+class Raven
+{
+    constructor()
+    {
+        this.width = 100
+        this.height = 100
+        this.x = canvas.width
+        this.y = Math.random() * (canvas.height * this.height)
+        this.directionX = Math.random() * 5 + 3
+        this.directionY = Math.random() * 5 - 2.5
+    }
+}
