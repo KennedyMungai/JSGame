6593fe97f3780edaa76a7d59b2fcdb61eeff005d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-15 16:15:19 +0300

    God rid of the wrong property

--- SideScroller/app.js
+++ SideScroller/app.js
@@ -100,7 +100,7 @@ const drawScore = () =>
 window.addEventListener('click', (e) =>
 {
     const detectPixelColor = collisionCtx.getImageData(e.x, e.y, 1, 1)
-    console.log(detectPixelColor.ImageData)
+    console.log(detectPixelColor)
 })
 
 const raven = new Raven()
