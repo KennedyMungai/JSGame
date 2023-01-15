bcf69b5c3e5eecd135dd67f711386815a4294132
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-15 13:10:54 +0300

    Console logge dout the timestamp

--- SideScroller/app.js
+++ SideScroller/app.js
@@ -41,6 +41,7 @@ const animate = (timestamp) =>
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     let deltaTime = timestamp - lastTime
     lastTime = timestamp
+    console.log(timestamp)
 
     requestAnimationFrame(animate)
 }
