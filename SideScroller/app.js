60f23e43685ccd7fb51d9deeebb7ed8450c76528
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-15 15:19:09 +0300

    Defined the size Modifier which is between 0.4 and 1

--- SideScroller/app.js
+++ SideScroller/app.js
@@ -17,6 +17,7 @@ class Raven
     {
         this.spriteWidth = 271
         this.spriteHeight = 194
+        this.sizeModifier = Math.random() * 0.6 + 0.4
         this.width = this.spriteWidth / 2
         this.height = this.spriteHeight / 2
         this.x = canvas.width
