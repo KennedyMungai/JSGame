32f21efc3b2ea3d62859a4a1b0b962a868b3f0d3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-16 15:57:12 +0300

    Defined the constructor, update and draw functions

--- SubClassing/app.js
+++ SubClassing/app.js
@@ -4,3 +4,21 @@ const canvas = document.getElementById('canvas1')
 const ctx = canvas.getContext('2d')
 canvas.width = 500
 canvas.height = 800
+
+class Game
+{
+    constructor()
+    {
+
+    }
+
+    update()
+    {
+
+    }
+
+    draw()
+    {
+
+    }
+}
